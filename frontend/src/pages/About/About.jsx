a48a4d0fb32063a0857afffcd02cecdba7a5a929
ali-zunega <ali.zunega@gmail.com>
2026-03-24 22:13:20 -0300

    feat: add About page and trajectory timeline

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,3 +1,11 @@
+import { AboutMe } from "../../components/AboutMe/AboutMe";
+import { Trajectory } from "../../components/AboutMe/Trajectory";
+
 export default function About() {
-    return <h1>About</h1>;
+  return (
+    <>
+      <AboutMe />
+      <Trajectory />
+    </>
+  );
 }
